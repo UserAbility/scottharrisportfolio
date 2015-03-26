769608ea873e387c82b9b521f9bd179c69f060dc
Scott Harris <scottharris@shuttercal.com>
2015-03-25 17:06:43 -0700

    first js test

---
+++ media/javascript/scottlib1.js
@@ -0,0 +1,7 @@
+var name = prompt("What is your name?");
+var home = prompt("What's your favorite car?");
+var thing = prompt("What's your favorite sex toy?");
+
+var sentance = "<h2>Did you hear about " + name + "? Who lived in a " + home + " with a freaking " + thing + "???</h2>";
+
+document.write(sentance);
